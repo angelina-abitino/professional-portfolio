7577908164fe7aa25cb6203ba15df59437936650
Angelina Abitino <angelina.abitino@gmail.com>
2023-03-24 10:43:31 -0400

    updated scatter chart icon

--- src/portfolio.js
+++ src/portfolio.js
@@ -79,7 +79,7 @@ https://fontawesome.com/icons?d=gallery */
     },
     {
       skillName: "Tableau",
-      fontAwesomeClassname: "far fa-chart-scatter"
+      fontAwesomeClassname: "fas far fa-chart-scatter-bubble"
     },
     {
       skillName: "Figma",
