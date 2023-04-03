fe0aae9952ce4fd409d0ec93a90f94b6979f8aff
Angelina Abitino <angelina.abitino@gmail.com>
2023-04-02 20:54:25 -0400

    removing progress bars

--- src/portfolio.js
+++ src/portfolio.js
@@ -130,7 +130,7 @@ const educationInfo = {
 // Your top 3 proficient stacks/tech experience
 
 const techStack = {
-  viewSkillBars: true, //Set it to true to show Proficiency Section
+  viewSkillBars: false, //Set it to true to show Proficiency Section
   experience: [
     {
       Stack: "SQL", //Insert stack or technology you have experience in
